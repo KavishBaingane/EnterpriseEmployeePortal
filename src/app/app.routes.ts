b2380c46233512375eb25eb1a79af5bd526753de
Kavish-Evince <kavish@evincedev.com>
2026-05-23 20:34:51 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./features/dashboard/dashboard/dashboard')
+                .then(m => m.Dashboard)
+    },
+
+    {
+        path: 'employees',
+        loadComponent: () =>
+            import('./features/employees/employee-list/employee-list')
+                .then(m => m.EmployeeList)
+    },
+
+    {
+        path: 'department',
+        loadComponent: () =>
+            import('./features/department/department/department')
+                .then(m => m.Department)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from './layout/components/navbar/navbar';
+import { Sidebar } from './layout/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Sidebar
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('EnterpriseEmployeePortal');
 }
